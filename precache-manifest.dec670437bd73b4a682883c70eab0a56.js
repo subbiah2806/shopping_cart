a29efa9f1f50f41af932beef14f99f5fb6195fa7
subbiah <subbiah_chandramouli@apple.com>
2019-11-09 00:57:25 -0800

    Updates

---
+++ precache-manifest.dec670437bd73b4a682883c70eab0a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea61cd37aeab0297a3a324e43d2c14e",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "a825641d26ef448c7ac2",
+    "url": "/shopping_cart/static/css/main.978ea6e2.chunk.css"
+  },
+  {
+    "revision": "364fa797943ddf0de236",
+    "url": "/shopping_cart/static/js/2.d86d737d.chunk.js"
+  },
+  {
+    "revision": "a825641d26ef448c7ac2",
+    "url": "/shopping_cart/static/js/main.468a8c71.chunk.js"
+  },
+  {
+    "revision": "09707ba48fd35d25ed47",
+    "url": "/shopping_cart/static/js/runtime-main.cfe558f3.js"
+  }
+]);
